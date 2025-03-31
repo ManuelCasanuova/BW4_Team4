69b431cd19c0a88a2851c948c9a0c6a817f52be9
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-03-31 19:35:29 +0200

    relations_titoliViaggio

--- src/main/java/titoloViaggio/TitoloViaggio.java
+++ src/main/java/titoloViaggio/TitoloViaggio.java
@@ -1,16 +1,37 @@
 package titoloViaggio;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sistemadistribuzione.SistemaEmettitore;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class TitoloViaggio {
-    //
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long codiceUnivoco;
     private LocalDate dataEmissione;
+    private boolean valido;
+
+    @ManyToOne
+    private SistemaEmettitore puntoEmittente;
+    // bisogna inserire OneToMany in sistemaEmettitore e mappare la colonna con titolo di viaggio
+
+    public TitoloViaggio(LocalDate dataEmissione, boolean valido, SistemaEmettitore puntoEmittente) {
+
+        this.dataEmissione = dataEmissione;
+        this.valido = valido;
+        this.puntoEmittente = puntoEmittente;
+    }
+
 
 }
