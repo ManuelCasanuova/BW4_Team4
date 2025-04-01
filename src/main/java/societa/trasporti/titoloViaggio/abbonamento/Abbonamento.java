bbf1705a4d770e4a3a657a04a1be01896f786b8e
“Piero793” <pierolacitignola7@gmail.com>
2025-04-01 17:51:25 +0200

    main+db

--- src/main/java/societa/trasporti/titoloViaggio/abbonamento/Abbonamento.java
+++ src/main/java/societa/trasporti/titoloViaggio/abbonamento/Abbonamento.java
@@ -5,24 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import societa.trasporti.titoloViaggio.TitoloViaggio;
 import societa.trasporti.utenti.Tessera;
-import societa.trasporti.utenti.Utente;
 import societa.trasporti.vendita.PuntoVendita;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name="abbonamenti")
+@DiscriminatorValue("ABBONAMENTO")
 @NoArgsConstructor
-
-
 public class Abbonamento extends TitoloViaggio {
 
-
+    @Enumerated(EnumType.STRING)
     @Column(name = "tipo_abbonamento", nullable = false)
-    @Enumerated
     private TipoAbbonamento tipoAbbonamento;
-
 
     @Column(name = "data_inizio", nullable = false)
     private LocalDate dataInizio;
@@ -34,14 +29,17 @@ public class Abbonamento extends TitoloViaggio {
     @JoinColumn(name = "tessera_id")
     private Tessera tessera;
 
-    public Abbonamento(Long codiceUnivoco, double prezzoViaggio, LocalDate dataAquisto, PuntoVendita puntoVendita, TipoAbbonamento tipoAbbonamento, LocalDate dataInizio, Tessera tessera) {
-        super(codiceUnivoco, prezzoViaggio, dataAquisto, puntoVendita);
-        this.dataInizio = dataInizio;
-        if (tipoAbbonamento == TipoAbbonamento.SETTIMANALE) this.dataFine = dataInizio.plusDays(7);
-        else if (tipoAbbonamento == TipoAbbonamento.MENSILE) this.dataFine = dataInizio.plusMonths(1);
-        else if (tipoAbbonamento== tipoAbbonamento.SETTIMANALE) this.dataFine = dataInizio.plusMonths(6);
-        else this.dataFine = dataInizio.plusYears(1);
-        this.tessera = tessera;
+    public Abbonamento(Long codiceUnivoco, double prezzoViaggio, LocalDate dataAcquisto, PuntoVendita puntoVendita,
+                       TipoAbbonamento tipoAbbonamento, LocalDate dataInizio, Tessera tessera) {
+        super(codiceUnivoco, prezzoViaggio, dataAcquisto, puntoVendita);
         this.tipoAbbonamento = tipoAbbonamento;
+        this.dataInizio = dataInizio;
+        this.tessera = tessera;
+
+        switch (tipoAbbonamento) {
+            case SETTIMANALE -> this.dataFine = dataInizio.plusDays(7);
+            case MENSILE -> this.dataFine = dataInizio.plusMonths(1);
+            case SEMESTRALE -> this.dataFine = dataInizio.plusMonths(6);
+        }
     }
 }
